f97848dba50cea7e1495abad819cfebd14c3b765
Linus Rachlis <linus@rachlis.net>
2014-12-04 14:56:03 -0500

    Convert to use AngularFire

--- js/waterfall.js
+++ js/waterfall.js
@@ -1,22 +1,12 @@
-var fb = new Firebase('https://blinding-inferno-9332.firebaseio.com/')
+var waterfall = angular.module('waterfall', ['firebase'])
 
-var waterfall = angular.module('waterfall', [])
-
-waterfall.controller('WaterfallController', ['$scope', '$timeout',
-  function($scope, $timeout) {
-    $scope.messages = []
-
-    fb.on('child_added', function (snap) {
-      $scope.$apply(function () {
-        $scope.messages.push(snap.val())
-      })
-    })
+waterfall.controller('WaterfallController', ['$scope', '$firebase',
+  function($scope, $firebase) {
+    var fb = $firebase(new Firebase('https://blinding-inferno-9332.firebaseio.com/'))
+    $scope.messages = fb.$asArray()
 
     $scope.toss = function () {
-      var newMessage = $scope.newMessage
-      $timeout(function() {
-        fb.push(newMessage)
-      }, 0)
+      $scope.messages.$add($scope.newMessage)
       $scope.newMessage = ''
     }
   }
