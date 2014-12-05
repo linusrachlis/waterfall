78600ecc358678f12eb7c3ac9187cb83a9b0eedc
Linus Rachlis <linus@rachlis.net>
2014-12-05 09:38:17 -0500

    Messages slide/fade away based on random scale factor
    - Use of filters smells. Would prefer model methods on Message.
    - Calling heartbeat() at 10 fps to animate. Tab uses only <10% CPU,
      is there a better way though?

--- js/waterfall.js
+++ js/waterfall.js
@@ -1,6 +1,20 @@
-var waterfall = angular.module('waterfall', ['firebase'])
-
-waterfall.controller('WaterfallController', ['$scope', '$firebase',
+angular.module('waterfall', ['firebase'])
+.filter('getFontSize', function () {
+  return function (message) {
+    return message.scaleFactor * 40 + 10
+  }
+})
+.filter('getPercentProgress', function () {
+  return function (message, now) {
+    return message.scaleFactor * (now - message.createdAt) * 0.0001
+  }
+})
+.filter('getOpacity', function () {
+  return function (message, now) {
+    return 1 - (message.scaleFactor * (now - message.createdAt) * 0.000001)
+  }
+})
+.controller('WaterfallController', ['$scope', '$firebase',
   function($scope, $firebase) {
     var fb = $firebase(new Firebase('https://blinding-inferno-9332.firebaseio.com/'))
     $scope.messages = fb.$asArray()
@@ -11,16 +25,23 @@ waterfall.controller('WaterfallController', ['$scope', '$firebase',
 
       // Generate random RGB
       for (i = 0; i < 3; i++) {
-        rgb.push(Math.round(Math.random()*255))
+        rgb.push(Math.round(Math.random() * 255))
       }
 
       $scope.messages.$add({
           text: $scope.newMessage
         , rgb: rgb
-        , fontSize: 10 + Math.round(Math.random()*40)
+        , scaleFactor: Math.random()
         , createdAt: Date.now()
       })
       $scope.newMessage = ''
     }
+
+    function heartbeat() {
+      $scope.$apply(function () {
+        $scope.now = Date.now()
+      })
+    }
+    setInterval(heartbeat, 100)
   }
 ])
